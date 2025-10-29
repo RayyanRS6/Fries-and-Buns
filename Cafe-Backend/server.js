f415cbd6225b5a375b91c94af694532121a24972
sudais0 <sudais124578@gmail.com>
2025-10-29 20:59:54 +0500

    I have changed the file structure of whole Project. AND added project backend under the name of Cafe-Backend.

---
+++ Cafe-Backend/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+
+
+
+const app = express();
+
+app.use(cors());    
+app.use(express.json());
+
+
+// app.get("/", (req, res) => {
+//   res.send("Backend is running!");
+// });
+
+app.get("/api/deals", (req, res) => {
+
+    const deals=[
+
+        {id:1, name:"Deal : 1", price:750},
+        {id:2, name:"Deal : 2", price:1250},
+        {id:3, name:"Deal : 3", price:1770},
+        {id:1, name:"Deal : 1", price:750},
+        {id:2, name:"Deal : 2", price:1250},
+        {id:3, name:"Deal : 3", price:1770},
+        {id:1, name:"Deal : 1", price:750},
+        {id:2, name:"Deal : 2", price:1250},
+        {id:3, name:"Deal : 3", price:1770},
+    ];
+
+    res.json(deals);
+});
+
+app.post("/api/order", (req, res) => {
+
+    orderData = req.body;
+
+    console.log("New Order Recevied", orderData);
+
+    res.json({ msj:"Order Has Been Recevied", order:orderData});
+});
+
+
+
+
+
+app.listen(5000, () => {
+  console.log("Server started on http://localhost:5000");
+});
+
+
